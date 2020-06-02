dcc8b18e0952a2a85aeb47665d59edd75dfde6a9
gnanadeepika5 <60079729+gnanadeepika5@users.noreply.github.com>
2020-06-01 18:19:59 -0700

    added a button to add items

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+//import { FormsModule } from ''
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppGroceryComponent } from './app-grocery/app-grocery.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppGroceryComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    // FormsModule,
+    // HttpModul
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppGroceryComponent]
 })
 export class AppModule { }
